8a15e9380547b28c79627087f550095c13d1806d
Michiel Hermes <michielhermes@colloids.nl>
2018-07-04 14:34:21 +0200

    Added optional color in FFT

--- docs/viscol.js
+++ docs/viscol.js
@@ -436,7 +436,7 @@ function viscol_core(myCanvas,onChange){
   var fftTexture = {};
   function makeFFTTexture(part){
     makeFFTCanvas(FFTSIZE,FFTSIZE);
-    drawFFTCanvasColor(FFTSIZE,FFTSIZE,part);
+    drawFFTCanvas(FFTSIZE,FFTSIZE,part);
     fftTexture=gl.createTexture();
     gl.bindTexture(gl.TEXTURE_2D,fftTexture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, fftCtx.canvas);
